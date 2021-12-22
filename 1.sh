b3915eb43f398f5b6d51ae7571f2a34d473d9c03
Faure Xavier <suizokukan@orange.fr>
2021-12-22 10:47:21 +0100

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,1 +1,28 @@
-echo "Excelsior !!!"
+echo "louisix12261270 / ArchLinux / v1"
+sfdisk /dev/sda < 1.sfdisk
+
+mkfs.ext4 /dev/sda2
+mkswap /dev/sda1
+
+mount /dev/sda2 /mnt
+swapon /dev/sda1
+
+pacstrap base linux linux-firmware nano
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt
+
+ln -sf /usr/share/zoneinfo/Region/City /etc/localtime
+
+hwclock --systohc
+
+echo "fr_FR.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+
+echo "LANG=fr_FR.UTF-8" > /etc/locale.conf
+echo "LC_MESSAGES=fr_FR.UTF-8" >> /etc/locale.conf
+
+echo "KEYMAP=fr-latin1" > /etc/vconsole.conf
+
+echo "louisix12261270" > /etc/hostname
