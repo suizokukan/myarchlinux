84c90966df5c3c9108540949016b4da20f31d12f
Faure Xavier <suizokukan@orange.fr>
2021-12-22 19:28:06 +0100

    Update 1a.sh

--- 1a.sh
+++ 1a.sh
@@ -1,30 +1,53 @@
 echo "============================================"
-echo "=== louisix12261270 / ArchLinux / 1a: v4 ==="
+echo "=== louisix12261270 / ArchLinux / 1a: v5 ==="
 echo "============================================"
 
+echo
 echo "[01] === sfdisk /dev/sda < 1.sfdisk ==="
-sfdisk /dev/sda < 1.sfdisk
-sleep 1
+echo
 
+touch data.sfdisk
+echo "label: dos" >> data.sfdisk
+echo "label-id: 0xfac4ed85" >> data.sfdisk
+echo "device: /dev/sda" >> data.sfdisk
+echo "unit: sectors" >> data.sfdisk
+echo "sector-size: 512" >> data.sfdisk
+echo "" >> data.sfdisk
+echo "/dev/sda1 : start=        2048, size=     4194304, type=82, bootable" >> data.sfdisk
+echo "/dev/sda2 : start=     4196352, size=   620946096, type=83" >> data.sfdisk
+
+sfdisk /dev/sda < data.sfdisk
+sleep 2
+
+echo
 echo "[02] === format ==="
+echo
 mkfs.ext4 -F /dev/sda2
 mkswap /dev/sda1
-sleep 1
+sleep 2
 
+echo
 echo "[03] === mount and swapon ==="
+echo
 mount /dev/sda2 /mnt
 swapon /dev/sda1
-sleep 1
+sleep 2
 
+echo
 echo "[04] === pacstrap ==="
+echo
 pacman -Syy
 pacstrap /mnt base linux linux-firmware nano
-sleep 1
+sleep 2
 
+echo
 echo "[05] === /mnt/etc/fstab ==="
+echo
 genfstab -U /mnt >> /mnt/etc/fstab
-sleep 1
+sleep 2
 
+echo
 echo "[06] === arch-chroot /mnt ==="
+echo
 arch-chroot /mnt
-sleep 1
+sleep 2
