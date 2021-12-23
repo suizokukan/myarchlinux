d6ac439070802f3d41e8334d0cd88f7652f51dcc
Faure Xavier <suizokukan@orange.fr>
2021-12-23 14:00:33 +0100

    Update 1a.sh

--- 1a.sh
+++ 1a.sh
@@ -1,5 +1,5 @@
 echo "============================================"
-echo "=== louisix12261270 / ArchLinux / 1a: v8 ==="
+echo "=== louisix12261270 / ArchLinux / 1a: v9 ==="
 echo "============================================"
 
 echo
@@ -44,7 +44,7 @@ cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.old
 reflector -c "fr" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
 cat /etc/pacman.d/mirrorlist
 
-pacstrap /mnt base linux linux-firmware nano networkmanager
+pacstrap /mnt base linux linux-firmware nano
 
 pacman -Sy --noconfirm archlinux-keyring
 pacman-key --noconfirm --populate archlinux
