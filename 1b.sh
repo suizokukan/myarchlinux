a353d18e4847d873ae638a0aa3bb01720b08e7f9
Faure Xavier <suizokukan@orange.fr>
2021-12-23 16:19:21 +0100

    Update 1b.sh

--- 1b.sh
+++ 1b.sh
@@ -1,5 +1,5 @@
 echo "============================================="
-echo "=== louisix12261270 / ArchLinux / 1b: v12 ==="
+echo "=== louisix12261270 / ArchLinux / 1b: v13 ==="
 echo "============================================="
 
 echo
@@ -50,23 +50,33 @@ echo "127.0.1.1	louisix12261270" >> /etc/hosts
 sleep 2
 
 echo
-echo "[08] === DHCP ==="
+echo "[08] === pacman update ==="
 echo
-pacman -S --noconfirm dhcpcd
-systemctl enable dhcpcd
+
+pacman -Sy --noconfirm archlinux-keyring
+pacman-key --noconfirm --populate archlinux
+pacman-key --noconfirm --refresh-keys
+pacman -Syu --noconfirm 
+pacman -Scc --noconfirm 
+pacman -Syuu --noconfirm 
+
+
+echo
+echo "[09] === network ==="
+echo
 
 pacman -S --noconfirm networkmanager
 systemctl enable NetworkManager
 sleep 2
 
 echo
-echo "[09] === root password ==="
+echo "[10] === root password ==="
 echo
 echo "root:e" | chpasswd
 sleep 2
 
 echo
-echo "[10] === GRUB ==="
+echo "[11] === GRUB ==="
 echo
 pacman -S --noconfirm grub os-prober
 grub-install /dev/sda
@@ -74,26 +84,26 @@ grub-mkconfig -o /boot/grub/grub.cfg
 sleep 2
 
 echo
-echo "[11] === networkmanager ==="
+echo "[12] === networkmanager ==="
 echo
 pacman -S --noconfirm networkmanager
 systemctl enable NetworkManager
 sleep 2
 
 echo
-echo "[12] === i3 and conky ==="
+echo "[13] === i3 and conky ==="
 echo
 pacman -S --noconfirm i3 conky
 sleep 2
 
 echo
-echo "[13] === emacs ==="
+echo "[14] === emacs ==="
 echo
 pacman -S --noconfirm emacs
 sleep 2
 
 echo
-echo "[??] === proguser ==="
+echo "[15] === proguser ==="
 echo
 useradd -m proguser
 echo "proguser:e" | chpasswd
