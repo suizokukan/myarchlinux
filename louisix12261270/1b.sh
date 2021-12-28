dbbf3c49ba3e89279b15cb795aec9fefa955ecee
Faure Xavier <suizokukan@orange.fr>
2021-12-28 20:18:46 +0100

    Update 1b.sh

--- louisix12261270/1b.sh
+++ louisix12261270/1b.sh
@@ -1,5 +1,5 @@
 echo "============================================="
-echo "=== louisix12261270 / ArchLinux / 1b: v41 ==="
+echo "=== louisix12261270 / ArchLinux / 1b: v42 ==="
 echo "============================================="
 
 echo
@@ -86,7 +86,7 @@ sleep 4
 echo
 echo "[B.12] === X, i3/lxdm/lxterminal/nm-applet/conky/nitrogen ==="
 echo
-pacman -S --noconfirm xorg xorg-xinit xterm i3 lxterminal lxdm network-manager-applet conky nitrogen
+pacman -S --noconfirm xorg xorg-xinit xorg-xrandr xterm i3 lxterminal lxdm network-manager-applet conky nitrogen
 systemctl enable lxdm
 
 # sur ArchBang live, pour trouver le driver à installer:
