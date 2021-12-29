e0880b1768a548b831c4418c7a2a1a74ed6ba539
Faure Xavier <suizokukan@orange.fr>
2021-12-29 11:35:54 +0100

    Update 1a.sh

--- louisix12261270/1a.sh
+++ louisix12261270/1a.sh
@@ -1,10 +1,21 @@
 echo "============================================="
-echo "=== louisix12261270 / ArchLinux / 1a: v27 ==="
+echo "=== louisix12261270 / ArchLinux / 1a: v28 ==="
 echo "============================================="
 
-echo
-echo "[A.01] === sfdisk /dev/sda < 1.sfdisk ==="
-echo
+TITLECOLOR='\033[0;34m'
+DEFAULTCOLOR='\033[0m'
+
+title () {
+  echo
+  echo -e "$(TITLECOLOR)$1$(DEFAULTCOLOR)"
+  echo
+}
+
+aftersection () {
+    sleep 4
+}
+
+title "[A.01] === sfdisk /dev/sda < 1.sfdisk ==="
 
 touch data.sfdisk
 echo "label: dos" >> data.sfdisk
@@ -17,25 +28,19 @@ echo "/dev/sda1 : start=        2048, size=     4194304, type=82, bootable" >> d
 echo "/dev/sda2 : start=     4196352, size=   620946096, type=83" >> data.sfdisk
 
 sfdisk /dev/sda < data.sfdisk
-sleep 4
+aftersection
 
-echo
-echo "[A.02] === format ==="
-echo
+title "[A.02] === format ==="
 mkfs.ext4 -F /dev/sda2
 mkswap /dev/sda1
-sleep 4
+aftersection
 
-echo
-echo "[A.03] === mount and swapon ==="
-echo
+title "[A.03] === mount and swapon ==="
 mount /dev/sda2 /mnt
 swapon /dev/sda1
-sleep 4
+aftersection
 
-echo
-echo "[A.04] === pacstrap ==="
-echo
+title "[A.04] === pacstrap ==="
 
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.old
 curl https://raw.githubusercontent.com/suizokukan/myarchlinux/main/louisix12261270/1.mirrorlist > 1.mirrorlist
@@ -46,20 +51,16 @@ pacman -Syy
 
 pacstrap /mnt base linux linux-firmware
 
-sleep 4
+aftersection
 
-echo
-echo "[A.05] === /mnt/etc/fstab ==="
-echo
+title "[A.05] === /mnt/etc/fstab ==="
 genfstab -U /mnt >> /mnt/etc/fstab
-sleep 4
+aftersection
 
-echo
-echo "[A.06] === arch-chroot /mnt ==="
-echo
+title "[A.06] === arch-chroot /mnt ==="
 curl https://raw.githubusercontent.com/suizokukan/myarchlinux/main/louisix12261270/1b.sh > 1b.sh
 cp 1b.sh /mnt
 
 echo "about to chroot on /mnt ..."
-sleep 5
+aftersection
 arch-chroot /mnt sh 1b.sh
